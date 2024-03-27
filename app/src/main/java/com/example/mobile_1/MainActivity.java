0215602659233cb4acb2785b8567ac402e556795
lllillly <qkdgywls0503@naver.com>
2024-03-27 16:02:06 +0900

    f

---
+++ app/src/main/java/com/example/mobile_1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.mobile_1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.fruit);
+    }
+}
